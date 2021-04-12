32b0d978524b868c67048119187d4bec4c76dd56
yejx <635493395@qq.com>
2021-04-12 21:03:16 +0800

    add backend web

---
+++ app/src/backend/nexus-web/app.py
@@ -0,0 +1,20 @@
+from sanic import Sanic
+from sanic.response import HTTPResponse, text
+from sanic.request import Request
+from sanic.response import text
+
+debug = True
+
+app = Sanic("nexus")
+# app.static("/static", "/path/to/directory")
+
+@app.get("/")
+async def hello_world(request: Request) -> HTTPResponse:
+    return text("Hello, world.")
+
+
+if __name__ == '__main__':
+    import multiprocessing
+    workers = multiprocessing.cpu_count()
+    print("works:", workers)
+    app.run(host='0.0.0.0', port=7000, workers=workers, access_log=debug)
